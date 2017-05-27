b401f3788e4e22a10a48f4affbd7eea95264cc2a
Fidel Castro <fc.fidel1995@gmail.com>
2017-05-26 23:05:52 -0400

    now img tag works :)

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-/**
- * Created by fidelcastro on 5/26/17.
- */
-
 let requestURL = 'https://api.github.com/users/fcfidel';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
@@ -9,18 +5,21 @@ request.responseType = 'json';
 request.send();
 
 request.onload = function() {
-    var githubInfo = request.response;
-    populateHeader(githubInfo);
-    //showGithubInfo(githubInfo);
+  var githubInfo = request.response;
+  populateHeader(githubInfo);
+  //showGithubInfo(githubInfo);
 }
 
 function populateHeader(jsonObj) {
-    var myH1 = document.createElement('h1');
+  var myH1 = document.createElement('h1');
 
-    myH1.textContent = jsonObj['login'];
-    image.appendChild(myH1);
+  //myH1.textContent = jsonObj['login'];
+  //myH1.textContent = `<img src="${jsonObj['avatar_url']}" />`;
+  //image.appendChild(myH1);
 
-    var myPara = document.createElement('p');
-    myPara.textContent = 'Name: ' + jsonObj['name'] + ' // Formed: ' + jsonObj['created_at'];
-   image.appendChild(myPara);
+  var myPara = document.createElement('p');
+  myPara.textContent = 'Name: ' + jsonObj['name'] + ' // Formed: ' + jsonObj['created_at'];
+  image.appendChild(myPara);
+  
+  document.querySelector('#my-image').src = jsonObj['avatar_url'];
 }
